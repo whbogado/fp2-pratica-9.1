325c875122b612f6d4dfb61d7f543807878cc0f7
Wilson <Wilson@200.134.10.158>
2014-09-01 12:28:19 -0300

    Corrigida conversão de bytes para MB.

--- src/main/java/Pratica91.java
+++ src/main/java/Pratica91.java
@@ -17,9 +17,9 @@ public class Pratica91 {
         String os = System.getProperty("os.name");
         System.out.println("Executando em " + os);
         System.out.println("Número de processadores: " + rt.availableProcessors());
-        System.out.println("Memória total (MB): " + rt.totalMemory() / 1024);
-        System.out.println("Memória livre (MB): " + rt.freeMemory() / 1024);
-        System.out.println("Memória máxima VM (MB): " + rt.maxMemory() / 1024);
+        System.out.println("Memória total (MB): " + rt.totalMemory() / 1024 / 1024);
+        System.out.println("Memória livre (MB): " + rt.freeMemory() / 1024 / 1024);
+        System.out.println("Memória máxima VM (MB): " + rt.maxMemory() / 1024 / 1024);
         
         Process editor;
         String cmd;
